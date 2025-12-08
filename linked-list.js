45ee9e9e45c1bd461e99b2fabaf7fe7fc66227ef
Min-byeonghyun <qudgus5125@naver.com>
2025-12-09 00:34:15 +0900

    linkedlist

---
+++ linked-list.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  findNode(index) {
+    let count = 0;
+    let currentNode = this.head;
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return currentNode;
+  }
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    if (index >= this.length) {
+      this.append(value);
+      return;
+    }
+    const newNode = new Node(value);
+    const prevNode = this.findNode(index - 1);
+    const nextNode = prevNode.next;
+    newNode.next = nextNode; //
+    prevNode.next = newNode;
+    this.length++;
+  }
+  remove(index) {
+    const prevNode = this.findNode(index - 1);
+    const removeNode = prevNode.next;
+    prevNode.next = removeNode.next;
+    this.length--;
+  }
+}
+
+const linkedList = new LinkedList("병현");
+linkedList.append("병현2");
+linkedList.append("병현3");
+
+console.log("LinkedList", linkedList);

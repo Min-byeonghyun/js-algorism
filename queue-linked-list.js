1902e76a79aa15a097d16b58947c01f36b13c450
Min-byeonghyun <qudgus5125@naver.com>
2025-12-11 00:17:43 +0900

    queue linked list

---
+++ queue-linked-list.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const oldLast = this.last;   //  oldlast = value: a next :{ value : b next : null}
+      oldLast.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+  }
+  dequeue() {
+    if (!this.first) return null;
+    const removedNode = this.first;
+
+    if (this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.length--;
+    return removedNode;
+  }
+  peek() {
+    return this.first;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue("A");
+queue.enqueue("B");
+queue.enqueue("C");
+
+console.log(queue);

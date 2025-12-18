02bb7b5554756dd23772aa4728f0c769298f590a
Min-byeonghyun <qudgus5125@naver.com>
2025-12-18 20:15:13 +0900

    DoubleLinkedList

---
+++ 7.js
@@ -0,0 +1,93 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoubleLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      prev: null,
+      next: null,
+    };
+    this.tail = this.head;
+    this.selected = null;
+    this.stack = [];
+  }
+  append(value, cursor) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    if (cursor === value) {
+      this.selected = newNode;
+    }
+  }
+  move(direction, count) {
+    let currentNode = this.selected;
+    for (let i = 0; i < count; i++) {
+      currentNode = currentNode[direction];
+    }
+    this.selected = currentNode;
+  }
+  remove() {
+    this.stack.push(this.selected);
+    const prevNode = this.selected.prev;
+    const nextNode = this.selected.next;
+
+    if (prevNode) {
+      prevNode.next = nextNode;
+    }
+    if (nextNode) {
+      nextNode.prev = prevNode;
+      this.selected = nextNode;
+    } else {
+      this.selected = prevNode;
+    }
+  }
+  recover() {
+    const recoverNode = this.stack.pop();
+    const prevNode = recoverNode.prev;
+    const nextNode = recoverNode.next;
+    if (prevNode) {
+      prevNode.next = recoverNode;
+    }
+    if (nextNode) {
+      nextNode.prev = recoverNode;
+    }
+  }
+}
+
+function solution(n, k, cmd) {
+  const result = Array.from({ length: n }, () => "O");
+
+  const linkedList = new DoubleLinkedList(0);
+  for (let i = 1; i < n; i++) {
+    linkedList.append(i, k);
+  }
+  for (const command of cmd) {
+    const [action, count] = command.split("");
+    if (action === "R") {
+      linkedList.move("next", count);
+    }
+    if (action === "L") {
+      linkedList.move("prev", count);
+    }
+    if (action === "D") {
+      linkedList.remove();
+    }
+    if (action === "U") {
+      linkedList.recover();
+    }
+  }
+  linkedList.stack.forEach((node) => (result[node.value] = "X"));
+
+  return result.join("");
+}
+
+console.log(solution(6, 2, ["D", "R 2", "D", "U"]));
+console.log(solution(5, 2, ["D", "D", "D"]));
+console.log(solution(8, 3, ["D", "D", "L 2", "D", "U"]));

cebc436ba85926e3aabd88a00288ca389b3d8edc
Min-byeonghyun <qudgus5125@naver.com>
2025-12-18 22:39:02 +0900

    bubbleSort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,19 +1,17 @@
 function bubbleSort(array) {
   const n = array.length;
 
-  for (let i=0; i < n ; i++) {
+  for (let i = 0; i < n; i++) {
     let swapped = false;
-    for(let j = 0 ; j < n - 1 - i; j++) {
+    for (let j = 0; j < n - 1 - i; j++) {
       if (array[j] > array[j + 1]) {
-        [array[j], array[j+1]] = [array[j+1], array[j]]
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
         swapped = true;
       }
     }
-    if( !swapped) break;
+    if (!swapped) break;
   }
   return array;
-
 }
 
 console.log(bubbleSort([5, 4, 1, 6, 7]));
-

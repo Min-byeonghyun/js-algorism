b09a38e2401ea92ba583b5aa0ff2f746765a60d3
Min-byeonghyun <qudgus5125@naver.com>
2025-12-26 17:29:05 +0900

    lank sort

---
+++ 8.js
@@ -0,0 +1,23 @@
+function solution(score) {
+  const sorted = score
+    .map((s, index) => [s, index])
+    .sort((a, b) => b[0] - a[0]);
+  console.log(sorted);
+
+  const n = score.length;
+  const answer = Array(n);
+
+  for (let rank = 0; rank < n; rank++) {
+    const index = sorted[rank][1];
+
+    if (rank < 3) {
+      answer[index] = `Top ${rank + 1}`;
+    } else {
+      answer[index] = `Rank${rank + 1}`;
+    }
+  }
+  return answer;
+}
+
+console.log(solution([95, 85, 90, 70]));
+console.log(solution([50, 100, 75]));

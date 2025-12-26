6532f981a5820b16c16cc94b45483cf6a70a3245
Min-byeonghyun <qudgus5125@naver.com>
2025-12-26 18:51:27 +0900

    sort

--- 9.js
+++ 9.js
@@ -1,10 +1,13 @@
 function solution(tickets) {
 
-  tickets.sort((a,b) => a[1] - b[1])
+  tickets.sort((a,b) => a[1] - b[1]) // 하차시각 기준으로 오름차순으로 정렬
   
   let inspections = 1; // 초기 단속횟수
-  let inspectionsTime = tickets[0][1];
+  let inspectionsTime = tickets[0][1]; // 첫 승객 하차시간
 
+
+  // 모든 승객을 순회하면서 현재 단속 시간(inspectionsTime)보다 해당 승객의 승차 시각 (tickets[i][0])이 늦다면
+  // 기존 단속시간으로 잡을수없으므로 단속횟수를 늘리고 기준시간을 갱신
   for(let i =0; i < tickets.length; i++) {
     if(tickets[i][0] > inspectionsTime) {
       inspections ++;
@@ -18,7 +21,3 @@ function solution(tickets) {
 console.log(solution([[8, 10], [3, 7], [1, 5], [2, 6]]));
 console.log(solution([[1, 2], [3, 4], [5, 6]]));
 
-// 2번째 배열요소로 오름차순 정렬 해서 첫번쨰 값부터 시작
-// inspections = 단속 횟수를 측정하는 변수를 선언
-// 2번째 요소를 inspectionTime 단속시간으로 맞추고
-// 만약 이 TICKETS배열의 첫번째 요소가 inspectionsTime보다 크다면 inspections를 올리고 inspectionTime을 바꿈

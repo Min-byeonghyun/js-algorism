748db4b3f3215d5027d68e47e4e50eb4345ba586
Min-byeonghyun <qudgus5125@naver.com>
2026-01-07 23:18:21 +0900

    bfs

---
+++ 16.js
@@ -0,0 +1,53 @@
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
+
+  const dx = [-1, 0, 1, 0]; // 상 우 하 좌 이동 배열 정의
+  const dy = [0, 1, 0, -1];
+
+  const d = Array.from({ length: n }, () => Array(m).fill(0));
+  const queue = [];
+
+  queue.push([0, 0]);
+  d[0][0] = 1;
+  maps[0][0] = 0;
+
+  while (queue.length) {
+    let [x, y] = queue.shift();
+
+    for (let k = 0; k < 4; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+
+      if (0 <= nx && nx < n && 0 <= ny && ny < m && maps[nx][ny] === 1) {
+        queue.push([nx, ny]);
+        maps[nx][ny] = 0;
+        d[nx][ny] = d[x][y] + 1;
+      }
+    }
+  }
+
+  return d[n - 1][m - 1] || -1;
+}
+console.log(
+  solution([
+    [1, 1, 0, 1],
+    [0, 1, 0, 1],
+    [0, 1, 1, 1],
+    [1, 0, 0, 1],
+  ])
+);
+console.log(
+  solution([
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+  ])
+);
+console.log(
+  solution([
+    [1, 0, 1],
+    [0, 0, 1],
+    [1, 1, 1],
+  ])
+);
